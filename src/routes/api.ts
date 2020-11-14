985a89cf711cbab95eb1041695dc778f8ee359b0
nguyenngoctai126 <nguyenngoctai1262@gmail.com>
2020-11-14 10:42:35 +0700

    updatePassword

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,18 @@ const router: Router= Router()
 
 import {apiController} from "../controllers/apiController";
 
+router.get('/getAllProduct', apiController.getAllProduct)
+router.get('/Rating', apiController.getAllFavorite)
+router.get('/getAllCustomer', apiController.getAllCustomer)
+router.post('/createCustomer', apiController.createCustomer)
+router.post('/createOrderDetail', apiController.createOrderDetail)
+router.post('/getOrderByID', apiController.getOrderByID)
+router.post('/deleteOrderDetail', apiController.deleteOrderDetail)
+router.post('/updateAmountOderDetail', apiController.updateAmountOderDetail)
+router.post('/updateOder', apiController.updateOder)
+router.post('/getOrderStatus', apiController.getOrderStatus)
+router.post('/getOrderIsSend', apiController.getOrderIsSend)
+
 router.post('/getVote', apiController.getAllVote)
 router.post('/signupuser', apiController.signupuser)
 router.post('/signinuser', apiController.signinuser)
