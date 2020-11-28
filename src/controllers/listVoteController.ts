d90d67222231b2d7873af1ed0e99194fe1406c7e
HuynhDuc <ducthph07859@fpt.edu.vn>
2020-11-28 14:02:12 +0700

    cập nhật api like and dislike. API trả anh dạng array

--- src/controllers/listVoteController.ts
+++ src/controllers/listVoteController.ts
@@ -1,10 +1,5 @@
 import {Request, Response} from 'express'
 import VoteModel, {Vote} from '../models/Vote'
-import OrderModel from "../models/Order";
-import ProductModel from "../models/Product";
-import os from "os";
-import multer from "multer";
-import {mongo} from "mongoose";
 class ListVoteController {
     public async index(request: Request, response: Response): Promise<void> {
         const listVote = await VoteModel.find().lean()
@@ -20,64 +15,6 @@ class ListVoteController {
             }
         }))
     }
-    // public async update(request: Request, response: Response): Promise<void> {
-    //     upload(request, response, (err) => {
-    //         if (err) {
-    //             response.send(err)
-    //             return
-    //         }
-    //
-    //         if (nameImage.toString() === "") {
-    //             ProductModel.findByIdAndUpdate(request.body.id,
-    //                 {
-    //                     name: request.body.nameProductUpdate,
-    //                     price: request.body.priceProductUpdate,
-    //                     amount: request.body.amountProductUpdate,
-    //                     brand: request.body.brandProductUpdate,
-    //                     size: request.body.sizeProductUpdate,
-    //                     tukhoa: request.body.tukhoaUpdate,
-    //                     // tslint:disable-next-line:no-shadowed-variable
-    //                 }, err => {
-    //                     if (err) {
-    //                         // tslint:disable-next-line:no-console
-    //                         response.send(err)
-    //                     } else {
-    //                         response.redirect('/product')
-    //                     }
-    //                 }
-    //             )
-    //
-    //         } else {
-    //             ProductModel.findByIdAndUpdate(request.body.id,
-    //                 {
-    //                     name: request.body.nameProductUpdate,
-    //                     price: request.body.priceProductUpdate,
-    //                     amount: request.body.amountProductUpdate,
-    //                     brand: request.body.brandProductUpdate,
-    //                     size: request.body.sizeProductUpdate,
-    //                     tukhoa: request.body.tukhoaUpdate,
-    //                     urlImage: 'uploads/' + nameImage
-    //                     // tslint:disable-next-line:no-shadowed-variable
-    //                 }, err => {
-    //                     if (err) {
-    //                         // tslint:disable-next-line:no-console
-    //                         response.send(err)
-    //                     } else {
-    //                         response.redirect('/product')
-    //                     }
-    //                 }
-    //             )
-    //         }
-    //         // const product: Product = new ProductModel({
-    //         //     name: request.body.nameProduct,
-    //         //     price: request.body.price,
-    //         //     amount: request.body.amount,
-    //         //     urlImage: 'uploads/'+nameImage
-    //         // })
-    //         // product.save();
-    //         nameImage = ''
-    //     })
-
     public async updateLike(request: Request, response: Response): Promise<void> {
         try {
             const idLike = request.body.idLike;
